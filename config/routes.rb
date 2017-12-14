c4c429a3a6a1776c5d3efe213b311a6ab96cbd31
nusound <uroshni77@gmail.com>
2017-12-14 10:27:54 +0100

    Created Quotes controller and json jbuilder file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
- # get 'pages/home'
 
   root to: "pages#home"
+
+  namespace :api, defaults: { format: :json } do
+    resources :quotes, only: [ :show ]
+  end
 end
